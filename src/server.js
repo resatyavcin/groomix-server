de1597ce4f8c478bd9e9b767de6c1e863fe65688
resatyavcin <resatyavcin@icloud.com>
2025-07-12 13:49:25 +0300

    fix: port edit

--- src/server.js
+++ src/server.js
@@ -73,6 +73,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("✅ Socket server çalışıyor: 3001");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
